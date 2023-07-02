6eed95e10b0e52ee66907c5ace7c5839ff67cdb1
Ubuntu <ubuntu@ip-172-31-0-252.ap-southeast-3.compute.internal>
2023-07-02 06:13:47 +0000

    Tambah Script Bing

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -38,6 +38,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google-site-verification" content="ypizvv-O13uxODO7bn6o6bVPUJryi-GfilHNHVNDn6E"/>
+    <meta name="msvalidate.01" content="8811EF3277673C96F571654D263258C7" />
     <title>Vilomina | {{ $title }}</title>
     {{-- Bootstrap CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
